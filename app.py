d1fae97d2f499130bca5f18df0d8fe90c83cbf18
Calum Wood <32903922+CalumWood@users.noreply.github.com>
2021-10-12 22:50:18 +0100

    added program and unit tests

---
+++ app.py
@@ -0,0 +1,5 @@
+def squared(x):
+    return x*x
+
+def cubed(x):
+    return x*x*x
---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+import app
+
+class TestMethods(unittest.TestCase):
+    def test_squared(self):
+        squareNumbers = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+        for i in range(1, 10):
+            self.assertEqual(app.squared(i), squareNumbers[i-1])
+
+    def test_cubed(self):
+        self.assertEqual(app.cubed(3), 9)
+
+if __name__ == '__main__':
+    unittest.main()
